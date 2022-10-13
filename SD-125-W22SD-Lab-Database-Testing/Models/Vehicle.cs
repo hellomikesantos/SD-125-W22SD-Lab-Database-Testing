631d7f982e726659336610dbc7967316f96745e0
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-10-13 17:31:26 -0500

    Add Licence prop to Vehicle

--- SD-125-W22SD-Lab-Database-Testing/Models/Vehicle.cs
+++ SD-125-W22SD-Lab-Database-Testing/Models/Vehicle.cs
@@ -5,6 +5,7 @@
         public int ID { get; set; }
         public int PassID { get; set; }
         public virtual Pass Pass { get; set; }
+        public string Licence { get; set; } 
         public virtual ICollection<Reservation> Reservations { get; set; }
         public bool Parked { get; set; }
     }
